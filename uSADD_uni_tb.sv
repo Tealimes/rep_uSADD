a13d2732a5dada94d2ab387d6d3b5f293f7fcb3b
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-06-14 18:53:36 -0400

    Finished base setup

--- uSADD_uni_tb.sv
+++ uSADD_uni_tb.sv
@@ -1,16 +1,50 @@
 `timescale 1ns/1ns
 `include "uSADD_uni.v"
+`include "sobolrng.v"
+`define TESTAMOUNT 10
+
+//used to check errors
+class errorCheck;
+    function new();
+        
+    endfunction //new()
+endclass 
 
 module uSADD_uni_tb();
+    parameter BITWIDTH = 8;
     logic iClk;
     logic iRstN;
-    logic [15:0] in;
-    logic out;
+    logic A;
+    logic [BITWIDTH-1:0] B;
+    logic loadB;
+    logic iClr;
+    logic oB;
+    reg [1:0] out;
 
-    uSADD_uni u_uSADD_uni(
+    //don't touch, used for bitstream generation inside tb
+    logic [BITWIDTH-1:0] sobolseq_tb;
+    logic [BITWIDTH-1:0] rand_a;
+
+    sobolrng #(
+        .BITWIDTH(BITWIDTH)
+    ) u_sobolrng_tb (
         .iClk(iClk),
         .iRstN(iRstN),
-        .in(in),
+        .iEn(1),
+        .iClr(iClr),
+        .sobolseq(sobolseq_tb)
+    );
+
+    uSADD_uni #(
+        .BITWIDTH(BITWIDTH)
+    ) u_uSADD_uni (
+        .iClk(iClk),
+        .iRstN(iRstN),
+        .A(A),
+        .B(B),
+        .loadB(loadB),
+        .iClr(iClr),
+        .oB(oB),
         .out(out)
     );
 
@@ -20,23 +54,31 @@ module uSADD_uni_tb();
         $dumpfile("uSADD_uni_tb.vcd"); $dumpvars;
 
         iClk = 1;
+        B = 0;
+        A = 0;
+        rand_a = 0;
         iRstN = 0;
+        iClr = 0;
+        loadB = 1;
 
-        in = 16'b1111111111111111;
-
-        #15;
+        #10;
         iRstN = 1;
 
-        #20;
-        in = 16'b1111111100000000;
+        //specified cycles of unary bitstreams
+        repeat(`TESTAMOUNT) begin
+            B = $urandom_range(255);
+            rand_a = $urandom_range(255);
 
-        #100;
-        in = 16'b1111000000000000;
+            repeat(256) begin
+                #10;
+                A = (rand_a > sobolseq_tb);
+            end
+        end
 
-        #100;
-        in = 16'b1111000000000000;
-        
-        #100;
+            
+        iClr = 1;
+        #400;
+
         $finish;
     end
     
