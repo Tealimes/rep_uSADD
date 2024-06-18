6c38e100bd32ef0c5db1e8a5a87b209fc2bcae8d
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-06-18 19:30:40 -0400

    cleaned up testbench with error checking

--- uSADD_uni_tb.sv
+++ uSADD_uni_tb.sv
@@ -1,38 +1,57 @@
-`timescale 1ns/1ns
 `include "uSADD_uni.v"
 `include "sobolrng.v"
 `define TESTAMOUNT 10
 
 //used to check errors
-class errorCheck;
+class errorcheck;
     real uResult;
     real eResult;
-    real calcNum;
-    real cntA;
-    real cntB;
-    real denom;
-    real sum;
-    real iMSE;
-    real iRMSE;
+    real fnum;
+    real cntrA;
+    real cntrB;
+    real fdenom;
+    real asum;
+    real mse;
+    real rmse;
+    static int i;
 
-    function new(real As, Bs, denom, calcNum);
-        cntA = As;
-        cntB = Bs;
-        denom = denom;
-        calcNum = calcNum;
-
-
-        
-    endfunction //new()
-
-    function mse();
-
+    function new();
+        asum = 0;
+        i = 0;
     endfunction
 
-    function rmse();
+    function addi(real A, B, denom, num);
+        cntrA = A;
+        cntrB = B;
+        fdenom = denom;
+        fnum = num;
+        i++;
+    endfunction 
 
+    function fSUM();
+        $display("Run %.0f results: ", i); 
+        uResult = (fnum/fdenom);
+        eResult = ((cntrA/fdenom) + (cntrB/fdenom)) / 2;
+
+        $display("uResult = %.9f", uResult);
+        $display("eResult = %.9f", eResult); 
+
+        asum = asum + ((uResult - eResult) * (uResult - eResult));
+        $display("sum: %.9f", asum);
     endfunction
-endclass 
+
+    function fMSE();
+        $display("Final Results: "); 
+        mse = asum / `TESTAMOUNT;
+        $display("mse: %.9f", mse);
+    endfunction
+
+    function fRMSE();
+        rmse = $sqrt(mse);
+        $display("rmse: %.9f", rmse);
+    endfunction
+
+endclass
 
 module uSADD_uni_tb();
     parameter BITWIDTH = 8;
@@ -44,21 +63,80 @@ module uSADD_uni_tb();
     logic iClr;
     reg oC;
 
-    //don't touch, used for bitstream generation inside tb
-    logic [BITWIDTH-1:0] sobolseq_tb1;
-    logic [BITWIDTH-1:0] sobolseq_tb2;
+    errorcheck error; //class for error checking
+    real num; //counts output's 1s
+    real cntA; //counts As
+    real cntB; //counts Bs
+    real denom; //denominator
+
+    //calculates end result
+    always@(posedge iClk or negedge iRstN) begin
+        if(~iRstN) begin
+            num <= 0;
+        end else begin
+            if(~iClr) begin 
+                num <= num + oC;
+            end else begin
+                num <= 0;
+            end
+        end
+    end
+
+    //calculates denominator
+    always@(posedge iClk or negedge iRstN) begin
+        if(~iRstN) begin
+            denom <= 0;
+        end else begin
+            if(~iClr) begin 
+                denom <= denom + 1;
+            end else begin
+                denom <= 0;
+            end
+        end
+    end
+
+    //Counts 1 in As and Bs
+    always@(posedge iClk or negedge iRstN) begin
+        if(~iRstN) begin
+            cntA <= 0;
+        end else begin
+            if(~iClr) begin 
+                cntA <= cntA + iA;
+            end else begin 
+                cntA <= 0;
+            end
+        end
+    end
+
+    //takes output of B from uMUL.v
+    always@(posedge iClk or negedge iRstN) begin
+        if(~iRstN) begin
+            cntB <= 0;
+        end else begin
+            if(~iClr) begin 
+                    cntB <= cntB + iB;
+            end else begin 
+                cntB <= 0;
+            end
+        end
+    end
+
+
+    //used for bitstream generation
+    logic [BITWIDTH-1:0] sobolseq_tbA;
+    logic [BITWIDTH-1:0] sobolseq_tbB;
     logic [BITWIDTH-1:0] rand_a;
     logic [BITWIDTH-1:0] rand_b;
 
     //generates the two stochastic bitstreams
     sobolrng #(
         .BITWIDTH(BITWIDTH)
-    ) u_sobolrng_tb1 (
+    ) u_sobolrng_tbA (
         .iClk(iClk),
         .iRstN(iRstN),
         .iEn(1),
         .iClr(iClr),
-        .sobolseq(sobolseq_tb1)
+        .sobolseq(sobolseq_tbA)
     );
 
     reg [BITWIDTH-1:0] iB_buff;
@@ -78,12 +156,12 @@ module uSADD_uni_tb();
 
     sobolrng #(
         .BITWIDTH(BITWIDTH)
-    ) u_sobolrng_tb2 (
+    ) u_sobolrng_tbB (
         .iClk(iClk),
         .iRstN(iRstN),
         .iEn(1),
         .iClr(iClr),
-        .sobolseq(sobolseq_tb2)
+        .sobolseq(sobolseq_tbB)
     );
 
     uSADD_uni #(
@@ -109,23 +187,33 @@ module uSADD_uni_tb();
         iRstN = 0;
         iClr = 0;
         loadB = 1;
+        error = new;
 
         #10;
         iRstN = 1;
 
         //specified cycles of unary bitstreams
         repeat(`TESTAMOUNT) begin
+            num = 0;
+            denom = 0;
+            cntA = 0;
+            cntB = 0;
             rand_b = $urandom_range(255);
             rand_a = $urandom_range(255);
 
             repeat(256) begin
                 #10;
-                iA = (rand_a > sobolseq_tb1);
-                iB = (iB_buff > sobolseq_tb2);
+                iA = (rand_a > sobolseq_tbA);
+                iB = (iB_buff > sobolseq_tbB);
             end
+
+            error.addi(cntA, cntB, denom, num);
+            error.fSUM();
         end
 
-            
+        error.fMSE();
+        error.fRMSE();
+
         iClr = 1;
         iA = 0;
         iB = 0;
