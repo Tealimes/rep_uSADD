98e19177d7e30229cc886c45b8b3ecd160788bcd
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-06-20 19:34:58 -0400

    changed output to wire

--- parallelcnt.v
+++ parallelcnt.v
@@ -1,13 +1,14 @@
+//By Alexander Peacock
+//email: alexpeacock56ten@gmail.com
+
 module parallelcnt (
     input wire iClk,
     input wire iRstN,
     input wire iA,
     input wire iB,
-    output reg [1:0] oC
+    output wire [1:0] oC
 );
 
-    always@(posedge iClk or negedge iRstN) begin
-        oC <= iA + iB;
-    end 
+    assign oC = iA + iB;
 
 endmodule
